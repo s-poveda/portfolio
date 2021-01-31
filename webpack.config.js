f79298fadbe52a798fd246e98fab0c8afe378a3d
sam p <spoveda545@gmail.com>
2021-01-31 18:05:06 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  resolve: {
+    mainFiles: ["index"]
+  },
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/docs",
+    filename: "index_bundle.js"
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
