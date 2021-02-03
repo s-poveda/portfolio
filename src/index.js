9c41580a2fc84aedb679e397183c159d6df42113
sam p <spoveda545@gmail.com>
2021-02-03 00:22:03 -0500

    styling and color palette. Made slide-in nav

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import './css/index.css';
 import $ from 'jquery';
 import imgSrc from './imgSrc';
 
+function initHeaderBehaviour() {
+	const
+		headerLimit = Math.floor($('header').height()),
+	 	navbar = $('nav'),
+	 	navHeight = Math.floor($(navbar).height());
+
+	window.onscroll = () => {
+		if (window.pageYOffset >= (headerLimit - (navHeight >> 1))) {
+			$(navbar).addClass('sticky');
+		} else {
+			$(navbar).removeClass('sticky');
+		}
+	};
+}
+
 function main() {
-  $('img.discord').attr('src', imgSrc.discord);
-  $('img.node').attr('src', imgSrc.node);
+  $('img.icon.discord').attr('src', imgSrc.discord).attr('alt', 'Discord icon');
+  $('img.icon.node').attr('src', imgSrc.node).attr('alt', 'NodeJS icon');
+	initHeaderBehaviour();
 }
 
 $(main);
