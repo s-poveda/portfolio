019c18e17e3ea6430b3407c7929cbd9d55caf530
sam p <spoveda545@gmail.com>
2021-02-03 01:04:31 -0500

    reformatted img object

--- src/imgSrc.js
+++ src/imgSrc.js
@@ -1,5 +1,18 @@
 export default {
-  discord: 'https://nektony.com/wp-content/uploads/2019/07/discord-icon.png',
-  node: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
-
-}
+  discord: {
+    url: 'https://nektony.com/wp-content/uploads/2019/07/discord-icon.png',
+    alt: 'discord icon'
+  },
+  nodeJS: {
+    url: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
+    alt: 'node icon'
+  },
+  linkedIn: {
+    url: 'https://www.republiccapitalaccess.com/wp-content/uploads/2014/07/linkedin-icon.png',
+    alt: 'linkedin icon'
+  },
+  gitHub: {
+    url: 'https://cdn2.iconfinder.com/data/icons/black-white-social-media/64/social_media_logo_github-128.png',
+    alt: 'github icon'
+  }
+};
